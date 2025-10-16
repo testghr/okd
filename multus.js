309171c262acb050c67027778316b6388bfe7198
testghr <johndoe@example.com>
2025-10-16 14:19:10 +0200

    if

--- multus.js
+++ multus.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const os = require('os');
+
+// Get the IP address of a specific interface
+const ifaceName = 'eth0'; // or 'en0', 'wlan0', etc.
+const interfaces = os.networkInterfaces();
+const iface = interfaces[ifaceName];
+
+if (!iface) {
+  console.error(`Interface ${ifaceName} not found`);
+  process.exit(1);
+}
+
+// Find the first IPv4 address (not internal)
+const address = iface.find(details => details.family === 'IPv4' && !details.internal);
+
+if (!address) {
+  console.error(`No suitable IPv4 address found for interface ${ifaceName}`);
+  process.exit(1);
+}
 
 // Create two instances of express
 const app1 = express();
@@ -14,8 +33,10 @@ app2.get('/', (req, res) => {
     res.send('Hello World from interface 2');
 });
 
+console.log(address.address);
+
 // Start the first server on address 127.0.0.1:3000
-app1.listen(3000, '0.0.0.0', () => {
+app1.listen(3000, address.address, () => {
     console.log('Server 1 running at http://127.0.0.1:3000/');
 });
 
